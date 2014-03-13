baf18171314884c0a37cff60c88ea67750cb8ec4
Martina <martina.malberti@cern.ch>
2014-03-13 19:08:50 +0100

    updates

--- VertexOptimization/interface/SimpleVertexAnalysis.h
+++ VertexOptimization/interface/SimpleVertexAnalysis.h
@@ -15,6 +15,8 @@
 #include <iostream>
 #include <fstream>
 #include "math.h"
+#include "TRandom.h"
+
 
 // ------------------------------------------------------------------------------------
 class SimpleVertexAnalysis : public StatAnalysis 
@@ -31,6 +33,12 @@ class SimpleVertexAnalysis : public StatAnalysis
     void Term(LoopAll&);
     
     void ReducedOutputTree(LoopAll &l, TTree * outputTree);
+    void GetBranches(TTree *t, std::set<TBranch *>& s ) ;
+
+    void FillReductionVariables(LoopAll& l, int jentry){};
+    bool SelectEventsReduction(LoopAll&, int){};
+    bool SkimEvents(LoopAll& l, int jentry){};
+
 
     virtual bool AnalyseEvent(LoopAll& l, Int_t jentry, float weight, TLorentzVector & gP4, 
 			      float & mass, float & evweight, int & category, int & diphoton_id,
@@ -41,20 +49,31 @@ class SimpleVertexAnalysis : public StatAnalysis
     
     TString minBiasRefName;
     int storeNVert;
+    bool runOnReducedNtuples;
+    double sigmaT;
 
 private:
+
+    TRandom *rnd;
+
     std::vector<std::string> vtxVarNames_;
     std::vector<float> vtxVars_;
     
     TFile * uFile_;
     TTree * uTree_;
-    TH1 * hMinBiasSpecturm_, *hHiggsSpecturm_, *hMinBiasRef_;
+    TH1 * hMinBias_, *hHiggs_, *hMinBiasRef_[4];
     bool isClosestToGen_, passCiC_;
     int   nPU_, nVert_, itype_;
-    float evWeight_, ksprob_;
+    float evWeight_, ksprob_[4];
+    vector<float> alpha02_, alpha03_, alpha04_,alpha05_;
     TLorentzVector *pho1_;
     TLorentzVector *pho2_;
     TLorentzVector *dipho_;
+    float dtof1_, dtof2_;
+    float tof1_, tof2_;
+    float tReco1_;
+    float tReco2_;
+    float vtxZ_;
     
     TTree *evTree_;
     Float_t dZTrue_, zRMS_, zTrue_;
